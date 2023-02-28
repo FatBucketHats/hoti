d8728ddb8b1a7d7cd392e0792ff45349159f66c1
Joseph Poata <jopo@GSNM-83HJ473.staff.vuw.ac.nz>
2023-02-28 13:31:39 +1300

    fixing shift invert 2

--- plot.py
+++ plot.py
@@ -14,23 +14,22 @@ i=3
 no_vert = 3
 side_len = 20
 
-
 #path = "/Users/jopo/Documents/23/work/ti/work_data/hoti_equilat_polygon"
-#paths = path+f"/e{no_vert}_len{side_len}_{i}.csv"
+#path = "../data"
 
-fig, ax = plt.subplots()
+#typ = f"/e{no_vert}_len{side_len}_{i}.csv"
+#typ = f"/{n}x{n}square_{i}.csv"
+
 
 p = np.zeros((n,n))
 # for i in np.arange(4):
 #     p += np.loadtxt(f"{n}x{n}square_{i}.csv", delimiter=",")
 
-path = "/Users/jopo/Documents/23/work/ti/work_data/hoti_equilat_polygon"
-paths = path+f"/e{no_vert}_len{side_len}_{i}.csv"
 for i in np.arange(4):
-    p += np.loadtxt(path+f"/e{no_vert}_len{side_len}_{i}.csv", delimiter=",")
-
+    p += np.loadtxt(path+typ, delimiter=",")
 
 # p = np.loadtxt(paths, delimiter=",")
+fig, ax = plt.subplots()
 im = ax.imshow(p)
 fig.colorbar(im, ax=ax, label='Colorbar')
 plt.show()
